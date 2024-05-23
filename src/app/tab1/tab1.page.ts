444fb7dd77d749dfa0a304c7c60bd7e02dfb8524
Isael Garcia <isaelgarcia180@gmail.com>
2024-05-23 11:03:51 -0300

    primeiro commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { PokeAPIService } from 'src/services/poke-api.service';
+import { ViaCEPService } from 'src/services/via-cep.service';
+
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  areaBuscarPokemon: string = '52011210';
+  areaBusca: any = {
+    bairro: '',
+    localidade: '',
+    logradouro: '',
+    uf: ''
+  };
+  pokemonData: any = null;
+
+  constructor(
+    private pokeAPIService: PokeAPIService,
+    private viaCEPService: ViaCEPService
+  ) {}
+  buscarPokemon() {
+    this.viaCEPService.getViaCEPService(this.areaBuscarPokemon).subscribe((value) =>{
+      this.areaBusca.logradouro = JSON.parse(JSON.stringify(value)) ['logradouro'];
+      this.areaBusca.bairro = ', ' + JSON.parse(JSON.stringify(value)) ["bairro"];
+      this.areaBusca.localidade = ' - ' + JSON.parse(JSON.stringify(value)) ['localidade'];
+      this.areaBusca.uf = '-' + JSON.parse(JSON.stringify(value)) ['uf'];
+    });
+    const pokemonId = Math.floor(Math.random() * 100) + 1;
+    this.pokeAPIService.getPokeAPIService(pokemonId).subscribe(data => {
+      this.pokemonData = data;
+    });
+  }
+  get pokemonImageUrl() {
+    if (this.pokemonData) {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.pokemonData.id}.png`;
+    }
+    return ''; // Ou uma URL de imagem de placeholder se não houver dados disponíveis
+  }
+}
